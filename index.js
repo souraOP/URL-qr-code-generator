6ed921db7b6c70e11d84e628955840dc9345cfa2
souraOP <sourasishmondal95@gmail.com>
2023-06-17 16:45:54 +0530

    adding files

---
+++ index.js
@@ -0,0 +1,29 @@
+import inquirer from 'inquirer';
+import qr from "qr-image";
+import fs from "fs";
+
+inquirer
+  .prompt([
+    {
+        "message": "Type our URL: ", "name": "URL"
+    }
+  ])
+  .then((answers) => {
+    const url = answers.URL;
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream('qr_image.png'));
+
+    fs.writeFile("myURL.txt", url, (err)=>{
+        if(err) throw err;
+        console.log("URL has been saved!");
+    });
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
+
+  
